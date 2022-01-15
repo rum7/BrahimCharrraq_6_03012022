b1d573de59cd6939cc72a2a1abd9985b9ee28699
brahim <b.charraq@gmail.com>
2022-01-15 14:20:51 +0100

    Like + dislike

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -4,12 +4,13 @@ const fs = require('fs');
 
 // Middleware de création de nouvelle sauce
 exports.createSauce = (req, res, next) => {
+    // On extrait l'object JSON de sauce et on le stocke les informations dans sauceObject
     const sauceObject = JSON.parse(req.body.sauce);
-    delete sauceObject._id;
+    delete sauceObject._id; /* On supprimer le mauvais id renvoyé par le frontend */
     // On crée notre nouvelle sauce
     const sauce = new Sauce({
-        ...sauceObject,
-        imageUrl: `${req.protocol}://${req.get("host")}/images/${req.file.filename}`,
+        ...sauceObject, /* On utilise l'opérateur spread pour copier les champs contenu dans le corps de la requête */
+        imageUrl: `${req.protocol}://${req.get("host")}/images/${req.file.filename}`, /* protocol = http(s) - host = racine du serveur */
         likes: 0,
         dislikes: 0,
         usersLiked: [],
@@ -23,11 +24,16 @@ exports.createSauce = (req, res, next) => {
 
 // Middleware de modification de sauce
 exports.updateSauce = (req, res, next) => {
-    const sauceObject = req.file ?
+    const sauceObject = req.file ? /* On utilise l'opérateur ternaire pour savoir si "req.file" existe ou non */
         {
+            // Le fichier existe, on récupère les informations de l'objet et on modifie l'url de l'image
             ...JSON.parse(req.body.sauce),
             imageUrl: `${req.protocol}://${req.get("host")}/images/${req.file.filename}`
-        } : { ...req.body };
+        } : { 
+            // Le fichier n'existe pas, on prend simplement le corps de la requête
+            ...req.body 
+        };
+    // On prend l'objet créé et on modifie son id de sorte à ce qu'il corresponde à l'id des paramètres de requête
     Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
     .then(() => res.status(200).json({ message: 'Sauce modifiée !' }))
     .catch((error) => res.status(400).json({ error }));
@@ -35,9 +41,14 @@ exports.updateSauce = (req, res, next) => {
 
 // Middleware de suppression de sauce
 exports.deleteSauce = (req, res, next) => {
+    // On identifie la sauce à supprimer
     Sauce.findOne({ _id: req.params.id })
     .then((sauce) => {
+        // On définit le nom du fichier à supprimer
         const filename = sauce.imageUrl.split('/images/')[1];
+        // À l'aide de la fonction unlink du package fs, 
+        // Le 1er argument correspond au chemin du fichier à supprimer 
+        // Le 2e argument est le callback: on supprime la sauce une fois le fichier supprimer
         fs.unlink(`images/${filename}`, () => {
             Sauce.deleteOne({ _id: req.params.id })
             .then(() => res.status(200).json({ message: 'Sauce supprimée !' }))
@@ -68,35 +79,39 @@ exports.likesASauce = (req, res, next) => {
   
     Sauce.findOne({ _id: req.params.id })
     .then((sauce) => {
+        /**/
         const alreadyLiked = sauce.usersLiked.find((id) => id === userId);
         const alreadyDisliked = sauce.usersDisliked.find((id) => id === userId);
-
-        console.log("--------------");
-        console.log("userId liked: "+userId);
-        console.log(sauce.usersLiked.find((id) => id === userId));
-
-        console.log("--------------");
-        console.log("userId disliked: "+userId);
-        console.log(alreadyDisliked);
+        /**/
 
         switch(likeStatement) {
             case 1:
                 sauce.likes++;
                 sauce.usersLiked.push(userId);
+                console.log("--------------");
+                console.log("YES! On vient d'avoir un like de "+userId);
                 break;
             case 0:
                 if(alreadyLiked) {
                     sauce.likes--;
                     sauce.usersLiked = sauce.usersLiked.filter((id) => id !== userId);
+                    console.log("--------------");
+                    console.log("On avait déjà un like de "+alreadyLiked);
+                    console.log("Du coup on l'a perdu...");
                 }
                 if(alreadyDisliked) {
                     sauce.dislikes--;
                     sauce.usersDisliked = sauce.usersDisliked.filter((id) => id !== userId);
+                    console.log("--------------");
+                    console.log("On avait déjà un dislike de "+alreadyDisliked);
+                    console.log("Fiou plus de dislike, maintenant faut like hein...");
                 }
                 break;
             case -1:
                 sauce.dislikes++;
                 sauce.usersDisliked.push(userId);
+                console.log("--------------");
+                console.log("Ah relou, on vient d'avoir un dislike de "+userId);
                 break;
         }
         sauce.save()
